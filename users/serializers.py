f4908d6b27eaa95450011501b04217e823b9dcb1
omersb <omersaidbuldukk@gmail.com>
2023-01-05 22:46:47 +0300

    signals added

--- users/serializers.py
+++ users/serializers.py
@@ -3,6 +3,9 @@ from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
+from dj_rest_auth.serializers import TokenSerializer
+
+
 class RegisterSerializer(serializers.ModelSerializer):
 
     email = serializers.EmailField(
